73bffa479b3d6dadcbaea5dd5eb7ef9e95255a82
Michal Borowiecki <mbor81@gmail.com>
2021-04-11 10:54:59 +0100

    improved navigation

--- src/main/kotlin/simple.kt
+++ src/main/kotlin/simple.kt
@@ -7,7 +7,16 @@ import org.w3c.dom.events.WheelEvent
 import three.js.*
 import kotlin.math.PI
 
+val earthRadius = 6378.137
+
 val Window.aspectRatio get() = innerWidth.toDouble() / innerHeight
+
+operator fun Number.minus(other: Double) = toDouble() - other
+operator fun Number.minus(other: Number) = toDouble() - other.toDouble()
+operator fun Number.plus(other: Double) = toDouble() + other
+operator fun Number.times(other: Double) = toDouble() * other
+operator fun Number.compareTo(other: Double) = toDouble().compareTo(other)
+
 external interface Options {
     var passive: Boolean
 }
@@ -41,7 +50,13 @@ fun wheelHandler(event: Event) {
     if (event is WheelEvent) {
         event.preventDefault()
         if (event.ctrlKey) {
-            camera.position.z += event.deltaY * 10
+            val direction = Vector3();
+            camera.getWorldDirection( direction );
+            val radius = focused.geometry.parameters.radius
+            val newPosition = camera.position.clone().add(direction.multiplyScalar((camera.position.clone().sub(focused.position).length() - focused.geometry.parameters.radius) * event.deltaY / -100))
+            if (newPosition.length() > radius + 100.0) {
+                camera.position.copy(newPosition)
+            }
         } else {
             camera.rotation.x += event.deltaY / PI / 100
             camera.rotation.y += event.deltaX / PI / 100
@@ -49,35 +64,34 @@ fun wheelHandler(event: Event) {
     }
 }
 
-operator fun Number.minus(other: Double) = toDouble() - other
-operator fun Number.plus(other: Double) = toDouble() + other
-
 val clock = Clock()
 val camera = PerspectiveCamera(75, window.aspectRatio, 1, 2e9).apply {
-    position.z = 10e3
+    position.z = earthRadius*2
 }
 
-val renderer = WebGLRenderer().apply {
+val renderer = WebGLRenderer((js("{}") as WebGLRendererParameters).apply{ antialias = false }).apply {
     document.body?.appendChild(domElement)
     setSize(window.innerWidth, window.innerHeight-4)
     setPixelRatio(window.devicePixelRatio)
 }
 val texLoader = TextureLoader()
-val earthTex = texLoader.load("1_earth_8k.jpg")
+val earthTex = texLoader.load("1_earth_16k.jpg")
 val moonTex = texLoader.load("8k_moon.jpg")
 val starsTex = texLoader.load("tycho_skymap.jpg")
-val earth = Mesh(SphereGeometry(6378.137, 100, 100), MeshStandardMaterial().apply { map = earthTex }).apply {
+val earth = Mesh(SphereGeometry(earthRadius, 100, 100), MeshStandardMaterial().apply{ map = earthTex }).apply {
     rotation.y = PI
-    scale.y = 6356.752/6378.137
-    repeat(5) {
-        val atmosphere = Mesh(SphereGeometry(6378.137 + 20 + 20*it, 100, 100), MeshStandardMaterial().apply {
+    scale.y = 6356.752/ earthRadius
+    repeat(10) {
+        val atmosphere = Mesh(SphereGeometry(earthRadius + 20 + 10*it, 100, 100), MeshStandardMaterial().apply {
             color = Color(0xffffff)
             transparent = true
-            opacity = 0.1
+            opacity = 0.05
         })
         add(atmosphere)
     }
 }
+
+var focused = earth
 val moon = Mesh(SphereGeometry(1738.1, 100, 100), MeshStandardMaterial().apply { map = moonTex }).apply {
     position.x = -300e3
     rotation.y = -PI/4
